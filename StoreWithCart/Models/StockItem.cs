d1f018825ba1ea67ebb8545b3ec781cd132ac010
Martin Davis <martin_davis3@yahoo.ca>
2020-05-24 14:12:58 -0600

    initial commit

---
+++ StoreWithCart/Models/StockItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StoreWithCart.Models
+{
+    public class StockItem
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Description Required")]
+        public string Description { get; set; }
+        [Required(ErrorMessage = "Price Required")]
+        public decimal Price { get; set; }
+        public int QuantityInCart { get; set; }
+    }
+}
