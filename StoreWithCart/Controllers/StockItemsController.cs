f34fc76b7220be1eb6bb042cbd2fa1ac03d8e90a
Martin Davis <martin_davis3@yahoo.ca>
2020-05-24 16:14:12 -0600

    add scaffolded controller

---
+++ StoreWithCart/Controllers/StockItemsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using StoreWithCart.Data;
+using StoreWithCart.Models;
+
+namespace StoreWithCart.Controllers
+{
+    public class StockItemsController : Controller
+    {
+        private readonly StockContext _context;
+
+        public StockItemsController(StockContext context)
+        {
+            _context = context;
+        }
+
+        // GET: StockItems
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.StockItem.ToListAsync());
+        }
+
+        // GET: StockItems/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var stockItem = await _context.StockItem
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (stockItem == null)
+            {
+                return NotFound();
+            }
+
+            return View(stockItem);
+        }
+
+        // GET: StockItems/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: StockItems/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Description,Price,QuantityInCart")] StockItem stockItem)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(stockItem);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(stockItem);
+        }
+
+        // GET: StockItems/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var stockItem = await _context.StockItem.FindAsync(id);
+            if (stockItem == null)
+            {
+                return NotFound();
+            }
+            return View(stockItem);
+        }
+
+        // POST: StockItems/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Description,Price,QuantityInCart")] StockItem stockItem)
+        {
+            if (id != stockItem.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(stockItem);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!StockItemExists(stockItem.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(stockItem);
+        }
+
+        // GET: StockItems/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var stockItem = await _context.StockItem
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (stockItem == null)
+            {
+                return NotFound();
+            }
+
+            return View(stockItem);
+        }
+
+        // POST: StockItems/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var stockItem = await _context.StockItem.FindAsync(id);
+            _context.StockItem.Remove(stockItem);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool StockItemExists(int id)
+        {
+            return _context.StockItem.Any(e => e.Id == id);
+        }
+    }
+}
